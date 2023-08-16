3bbec49fa53e4c2c5f5cc96a1428af13e9e5cc9a
mourisl <mourisl@hotmail.com>
2023-08-16 11:51:12 -0400

    Fix several bugs in the script

--- SRA/SelectSra.pl
+++ SRA/SelectSra.pl
@@ -60,13 +60,15 @@ foreach my $tid (keys %taxTree)
 #Run,ReleaseDate,LoadDate,spots,bases,spots_with_mates,avgLength,size_MB,AssemblyName,download_path,Experiment,LibraryName,LibraryStrategy,LibrarySelection,LibrarySource,LibraryLayout,InsertSize,InsertDev,Platform,Model,SRAStudy,BioProject,Study_Pubmed_id,ProjectID,Sample,BioSample,SampleType,TaxID,ScientificName,SampleName,g1k_pop_code,source,g1k_analysis_group,Subject_ID,Sex,Disease,Tumor,Affection_Status,Analyte_Type,Histological_Type,Body_Site,CenterName,Submission,dbgap_study_accession,Consent,RunHash,ReadHash
 #SRR25408000,2023-07-24 21:05:20,2023-07-24 21:03:56,417481,201004449,417481,481,105,,https://sra-downloadb.be-md.ncbi.nlm.nih.gov/sos4/sra-pub-zq-7/SRR025/25408/SRR25408000/SRR25408000.lite.1,SRX21144088,WGS_05142019,WGS,RANDOM,GENOMIC,PAIRED,0,0,ILLUMINA,Illumina MiSeq,SRP450441,PRJNA996741,,996741,SRS18407838,SAMN36698470,simple,53408,Pseudomonas citronellolis,1,,,,,,,no,,,,,UCLA,SRA1678671,,public,F16C16AB8ECAC92FF88C93D7D6D04143,96E0E76C6B58FAF4539116CD0767D656
 my $minFrag = 1000000 ;
+my $maxFrag = 5000000 ;
 my $selected = 0 ;
 open FP, $ARGV[0] ;
+my %used ;
 while (<FP>)
 {
   chomp ;
   my @cols = split /,/, $_ ;
-  next if ($cols[3] < $minFrag) ;
+  next if ($cols[3] < $minFrag || $cols[3] > $maxFrag) ;
   my $taxId = $cols[27] ;
   next if (!defined $taxRank{$taxId}) ;
   
@@ -74,19 +76,30 @@ while (<FP>)
   {
     next if (!defined $taxToSpecies{$taxId} 
         || !defined $effectiveTax{ $taxToSpecies{$taxId}}) ;
+    next if (defined $used{$taxToSpecies{$taxId}}) ;
   }
   else
   {
     # Skip if its species is in the database
     next if ((defined $taxToSpecies{$taxId} 
-        && !defined $effectiveTax{ $taxToSpecies{$taxId}})) ;
+        && defined $effectiveTax{ $taxToSpecies{$taxId}})) ;
     
     next if (!defined $taxToGenus{$taxId} 
         || !defined $effectiveTax{ $taxToGenus{$taxId}}) ;
-
+    next if (defined $used{$taxToGenus{$taxId}}) ;
   }
-  print $cols[0] ;
+  
+  print $cols[0]."\n" ;
   ++$selected ;
+  
+  if ($inSpecies == 1)
+  {
+    $used{$taxToSpecies{$taxId}} = 1 ;
+  }
+  else
+  {
+    $used{$taxToGenus{$taxId}} = 1 ;
+  }
   last if ($selected >= $n) ;
 }
 close FP ;
